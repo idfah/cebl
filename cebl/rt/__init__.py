4bf91524d39de8c09009e1550c4cdf64da6b4b0f
Elliott M Forney <elliott.forney@gmail.com>
2017-06-29 17:43:12 -0600

    Forced matplotlib to use WXAgg backend in startup script and to attempt switching backends in rt.__init__ in case started from console.

--- cebl/rt/__init__.py
+++ cebl/rt/__init__.py
@@ -1,3 +1,10 @@
+# hack to prevent segfault in wxPython3.x, forces wxAgg backend XXX - idfah
+# https://github.com/matplotlib/matplotlib/issues/3316
+# note, the cebl startup script also sets the backend.  This is just in case
+# cebl is started from the console.
+import matplotlib.pyplot as plt
+plt.switch_backend('WXAgg')
+
 from . import events
 from . import logging
 from . import main
